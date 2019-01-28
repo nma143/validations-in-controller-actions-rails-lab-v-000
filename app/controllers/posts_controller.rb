3a0e78f5da422bd40232a0e1ac69e311abbd2a85
Nikki A <nikki.apostolakis@gmail.com>
2019-01-28 15:07:29 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -10,6 +10,12 @@ class PostsController < ApplicationController
   def update
     @post.update(post_params)
 
+    if @post.valid?
+      @post.save
+      redirect_to post_path(@post)
+    else
+      render :edit
+    end
     redirect_to post_path(@post)
   end
 
